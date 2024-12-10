8a98f9b0c171a16645a6b17f56216bd341653a7e
leigh-pozibl <leigh.maidment@pozibl.com>
2024-12-10 14:27:23 +0800

    Update action.sh
    temporary trial

--- action.sh
+++ action.sh
@@ -19,7 +19,8 @@ npm init -y && npm install -y postcss postcss-cli autoprefixer
 
 echo '🤵 Install Hugo'
 mkdir tmp/ && cd tmp/
-curl -sSL $(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*\hugo_extended_0.*\_Linux-64bit.tar.gz" | rev | cut -d ' ' -f 1 | rev | tr -d '"') | tar -xvzf-
+#curl -sSL $(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep "browser_download_url.*\hugo_extended_0.*\_Linux-64bit.tar.gz" | rev | cut -d ' ' -f 1 | rev | tr -d '"') | tar -xvzf-
+curl -sSL https://github.com/gohugoio/hugo/releases/download/v0.134.1/hugo_extended_0.134.1_Linux-64bit.tar.gz | tar -xvzf-
 mv hugo /usr/local/bin/
 cd .. && rm -rf tmp/
 cd ${GITHUB_WORKSPACE}
